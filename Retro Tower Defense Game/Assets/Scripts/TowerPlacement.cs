741ea330bc3b0296d63c90d0f68c5234c099fea2
Moon <uzmanshafifassy@gmail.com>
2023-08-22 17:14:12 +1000

    Tower Spawns but does not place

--- Retro Tower Defense Game/Assets/Scripts/TowerPlacement.cs
+++ Retro Tower Defense Game/Assets/Scripts/TowerPlacement.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TowerPlaccement : MonoBehaviour
+public class TowerPlacement : MonoBehaviour
 {
     public GameObject towerPrefab;
     private GameObject currentTower;
     private bool placingTower = false;
 
-    public Tilemap tilemap;
+    public Tilemap groundTilemap; 
+    public Tilemap topTilemap;
 
-    void Update()
+    private void Start()
+    {
+        groundTilemap = GetComponentInChildren<Tilemap>();
+        topTilemap = GetComponentInChildren<Tilemap>();
+    }
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            ToggleTowerSpawn();
+            ToggleTowerPlacementMode();
         }
 
         if (placingTower)
         {
-            FollowWithMouse();
+            MoveTowerWithCursor();
             if (Input.GetMouseButtonDown(0))
             {
                 PlaceTower();
@@ -28,7 +34,7 @@ public class TowerPlaccement : MonoBehaviour
         }
     }
 
-    private void ToggleTowerSpawn()
+    private void ToggleTowerPlacementMode()
     {
         placingTower = !placingTower;
 
@@ -46,7 +52,7 @@ public class TowerPlaccement : MonoBehaviour
         }
     }
 
-    private void FollowWithMouse()
+    private void MoveTowerWithCursor()
     {
         if (currentTower != null)
         {
@@ -61,28 +67,29 @@ public class TowerPlaccement : MonoBehaviour
     {
         if (currentTower != null)
         {
-            Vector3Int cellPos = tilemap.WorldToCell(currentTower.transform.position);
+            Vector3Int cellPosition = groundTilemap.WorldToCell(currentTower.transform.position);
 
-            if (!IsCellOccupied(cellPos))
+            if (!IsCellOccupied(cellPosition))
             {
-                Instantiate(towerPrefab, currentTower.transform.position, Quaternion.identity);
+                Instantiate(towerPrefab, groundTilemap.GetCellCenterWorld(cellPosition), Quaternion.identity);
                 Destroy(currentTower);
             }
         }
     }
 
-    private bool IsCellOccupied(Vector3Int cellPos)
+    private bool IsCellOccupied(Vector3Int cellPosition)
     {
-        TileBase tile = tilemap.GetTile(cellPos);
+        TileBase groundTile = groundTilemap.GetTile(cellPosition);
+        TileBase topTile = topTilemap.GetTile(cellPosition);
 
-        if (tile != null)
+        if (groundTile != null && topTile == null)
         {
-            if (tilemap.GetTileFlags(cellPos) == TileFlags.None)
+            if (groundTilemap.GetTileFlags(cellPosition) == TileFlags.None)
             {
                 return true;
             }
         }
-        
+
         return false;
     }
 }
