923dd087f7b2b4584fc81cebf31a0b545f379f7d
uzman <uzmanshafifassy@gmail.com>
2023-09-22 13:18:24 +1000

    merging my changes

--- Retro Tower Defense Game/Assets/Scripts/GameManager.cs
+++ Retro Tower Defense Game/Assets/Scripts/GameManager.cs
@@ -41,36 +41,52 @@ public class GameManager : MonoBehaviour
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Projectile"), LayerMask.NameToLayer("DonkeyKongProjectile"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Projectile"), LayerMask.NameToLayer("PongProjectile"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Projectile"), LayerMask.NameToLayer("Projectile"));
-        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Pathing"), LayerMask.NameToLayer("Projectile")); 
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Pathing"), LayerMask.NameToLayer("Projectile"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Pathing"), LayerMask.NameToLayer("PongProjectile"));
     }
 
     // Update is called once per frame. It doesn't matter what order this is called as after the first cycle the values will remain the same most of the time, unless a tower is moved/sold/upgraded. That does mean that we could actually only call this on those events, but that can happen another day
     void Update()
     {
-       applyBuffs();
+        applyBuffs();
     }
 
-    void applyBuffs() {
-        foreach (var tower in AllTowers) {
-            
+    public void RemoveTower(Tower tower)
+    {
+        if (_allTowers.Contains(tower))
+        {
+            _allTowers.Remove(tower);
+        }
+    }
+
+
+    void applyBuffs()
+    {
+        foreach (var tower in AllTowers)
+        {
+
             tower.cooldown = tower.base_cooldown;
             tower.range = tower.base_range;
             tower.damage = tower.base_damage; //tower doesn't have the damage on it, that must be on projectile somehow. Not sure how to make that work
 
             PowerPointTower ppt;
             //Get all the powerpoint towers using unity magic otherwise just do this
-            foreach (var powerpoint_tower in AllTowers) {
-                if (powerpoint_tower.TryGetComponent<PowerPointTower>(out ppt)) { //pseudocode
+            foreach (var powerpoint_tower in AllTowers)
+            {
+                if (powerpoint_tower.TryGetComponent<PowerPointTower>(out ppt))
+                { //pseudocode
 
                     //if in range use powerpoint range
-                    if (withinRange((Vector2)tower.transform.position, (Vector2)ppt.transform.position, ppt.range)) {
+                    if (withinRange((Vector2)tower.transform.position, (Vector2)ppt.transform.position, ppt.range))
+                    {
                         tower.cooldown *= ppt.cooldown_multipler;
                         tower.range *= ppt.range_multipler;
                         tower.damage *= ppt.damage_multiplier;
                     }
 
-                } else {
+                }
+                else
+                {
                     continue;
                 }
             }
