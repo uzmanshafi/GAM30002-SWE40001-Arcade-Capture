4c0b0b70f89383f400a65f20fb46a8c7f41a117d
Bill-Owen <bill@thankyoubox.com.au>
2023-10-23 14:18:00 +1100

    Added donkey kong pseudocode

--- Retro Tower Defense Game/Assets/Scripts/DonkeyKongTower.cs
+++ Retro Tower Defense Game/Assets/Scripts/DonkeyKongTower.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Utils;
 
 public class DonkeyKongTower : PredictiveTower
 {
@@ -11,6 +12,25 @@ public class DonkeyKongTower : PredictiveTower
     {
         base.initialise();
 		currentLevel = upgradeLevel;
+
+		// Find closest point on path
+
+		float nearest_point_distance_squared = float.MaxValue;
+		Vector2 nearest_point? = null;
+
+		// Loop through waypoints
+		for (int i = 0; i < waypoints.length - 1; ++i) {
+			// For each waypoint segment, get nearest point from tower position.
+			point = nearestPointOnSegment(this.position, waypoints[i], waypoints[i+1]);
+			// If distance is smaller than nearest_point_distance then update it as well as nearest_point
+			if ((this.position - point).sqrMagnitude < nearest_point_distance_squared) {
+				nearest_point = point;
+				nearest_point_distance_squared = (this.position - point).sqrMagnitude;
+			}
+		}
+
+		//Set the targetting position to be nearest_point instead of using predictive__projectile
+
 	}
 
 
